bf345ac34f77d3cc88ada5290b71cadb139ed07f
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-24 13:16:58 +0000

    Added new file with data moved over from basecontroller
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tesseract/trunk@31444 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_tables.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+	// This array is meant to store the list of the various controller tables
+	// used by the Tesseract components
+	// This list is used - in particular - by Data Consumers, in the BE,
+	// to find back to which Data Providers they relate so that operations
+	// such as mapping database fields to placeholders can take place
+	// This is necessary because - in such cases - the process is not initiated
+	// by a controller
+if (!isset($T3_VAR['EXT']['tesseract']['controller_mm_tables'])) {
+	$T3_VAR['EXT']['tesseract']['controller_mm_tables'] = array();
+}
+?>
